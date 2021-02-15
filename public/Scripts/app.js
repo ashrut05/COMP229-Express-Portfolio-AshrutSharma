09e992eccae6f19f2a7574c933e3b5c9146917e5
Ash Sharma <ashrut05@protonmail.com>
2021-02-14 23:19:23 -0500

    Initial Commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,16 @@
+/*
+Filename: 
+Student Name: Ashrut Sharma
+Student ID: 301107609 
+Date: 14th February, 2021
+*/
+
+//Outputs the message"App started..." on the console when app starts
+(function(){
+    function Start(){
+        console.log("App Started...")
+    }
+
+    window.addEventListener("load",Start);
+
+})();
---
+++ routes/index.js
@@ -0,0 +1,29 @@
+let express = require('express');
+let router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('home', { title: 'Home' });
+});
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'Home' });
+});
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Services' });
+});
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
+});
+
+module.exports = router;
